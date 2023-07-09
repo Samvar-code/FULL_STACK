03858ca1ee98d2f7d4ab5a106e6c82f91990c74e
Samvarthini Vedapuri <81792424+Samvar-code@users.noreply.github.com>
2023-07-09 09:59:10 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Desc from './components/desc';
-import Price from './components/price';
-import Image from './components/image';
-import Logo from './components/logo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Logo/>
-    <Desc/>
-    <Price/>
-    <Image/>
   </React.StrictMode>
 );
 
